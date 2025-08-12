d587149067e61237dc40d4c4b40c4a5468d9ec77
김정현 <shshjang14@gmail.com>
2025-08-12 16:41:29 +0900

    더미 파일 삭제

--- main.py
+++
@@ -1,9 +0,0 @@
-from fastapi.middleware.cors import CORSMiddleware
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # 프론트엔드 주소
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
